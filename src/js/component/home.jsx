08d004098d551a231e71c6a5a556041af731de7d
amelv-03 <aiden.melv03@gmail.com>
2024-10-07 22:10:05 +0000

    yuh

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [todoList, setTodoList] = useState([])
+    const [userInput, setUserInput] = useState("")
+
+    const addTodo = () => {
+        let task = {label: userInput, is_done: false};
+        setTodoList([...todoList, task])
+    }
+    const deleteTodo = (todo) => {
+        let updatedTodoList = todoList.filter((task, index) => {
+            return task.label !== todo
+        })
+        setTodoList(updatedTodoList)
+    }
+    return (
+        <div className="text-center">
+            <input onChange={(ev) => setUserInput(ev.target.value)}/>
+            <button className="btn btn-info" onClick={() => addTodo()}>Add Todo</button>
+
+            {todoList?.map((task, index) => (
+                <div>{task.label}<span onClick={() => deleteTodo(task.label)}>DELETE</span></div>
+            ))}
+        </div>
+    );
 };
 
 export default Home;
